c1f2282ef0d47f97578efae81f6435f4b1cd8f6f
liujunchao <liu_james_cn@163.com>
2016-04-02 10:40:03 +0800

    modify

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 本项目主要尝试chrome的api开发，熟悉api及语法。
 1，配置background->scripts->JS路径  可配置CHROME后台运行脚本
+2, 配置permissions->contextMenus可以配置生成右键菜单权限
+3, ContentScript，可与当前页面在同一个空间，而BackgroundScript则是隔离开的。在配置文件中加入content_scripts项。
--- scripts/background.js
+++ scripts/background.js
@@ -3,3 +3,58 @@ setInterval(function() {
    console.log("I have already running :"+seconds);
    seconds++;
 }, 1000);
+
+
+chrome.contextMenus.create({
+    type:"normal",
+    title:"menu a",
+    id:'a'
+});
+
+chrome.contextMenus.create({
+    type:"radio",
+    title:"menu b",
+    id:"b",
+    checked:true
+});
+
+chrome.contextMenus.create({
+    type:"checkbox",
+    title:"menu c",
+    id:"c",
+    checked:true
+});
+
+chrome.contextMenus.create({
+    type:"separator"
+});
+
+chrome.contextMenus.create({
+    type:"normal",
+    title:"menu e",
+    id:"e",
+    parentId:'a'
+});
+
+chrome.contextMenus.create({
+    type:"normal",
+    title:"menu g",
+    id:"g",
+    parentId:'a'
+});
+
+//配置当对链接右键的时候显示以下这个菜单
+chrome.contextMenus.create({
+    type:"normal",
+    title:"show  after right click on link",
+    contexts:["link"]
+});
+//配置对选中的文本右键的时候显示以下这个菜单
+chrome.contextMenus.create({
+    type:"normal",
+    title:"show after selecting text",
+    contexts:["selection"],
+    onclick:function (info,tab) {
+        alert("select text "+info.selectionText);
+    }
+});
